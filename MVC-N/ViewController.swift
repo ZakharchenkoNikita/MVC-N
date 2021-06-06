d1f19812dad0c32df20fb3ae970828dfc1d0ebbe
Nikita <immortalvelial@icloud.com>
2021-06-06 17:58:10 +0200

    Initial Commit

---
+++ MVC-N/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Nikita on 06.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
